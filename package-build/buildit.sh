df1bfec0343117f5850f09a489ce404b08e44d7e
J.C. Woltz <jwoltz@gmail.com>
2011-10-03 14:43:27 -0400

    add a ignore file so that built zip files and images are not committed into git modify the buildit script to build two images: br1105-main br-e10-next

--- package-build/buildit.sh
+++ package-build/buildit.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 tld="/usr/local/src/git/E10-buildroot"
-uinitk="$tld/br-e10-recv/output/images/uImage"
 ukernel="$tld/br1105-main/output/images/uImage"
 urootfs="$tld/br1105-main/output/images/rootfs.jffs2"
+nkernel="$tld/br-e10-next/output/images/uImage"
+nrootfs="$tld/br-e10-next/output/images/rootfs.jffs2"
+uinitk="$tld/br-e10-recv/output/images/uImage"
 uscript="$tld/br-e10-recv/jcmod/root/upgrade.sh"
 ufilesd="$tld/package-build/files"
 uarchived="$tld/package-build/archive"
+
+echo "Cleaning old files..."
+rm $ufilesd/*
 
 echo "Copying files..."
 cp $uinitk $ufilesd/uImage-e10i
@@ -17,11 +22,24 @@ cp $uscript $ufilesd/
 echo "Compiling u-boot environment script"
 mkimage -T script -C none -n 'E10 environment update' -A arm -d set-uboot-env files/setenv.img
 
-FILENAME=e10-`date +%Y%m%d%H%M%S`
-echo "Creating zip file..."
+FILENAMES=e10-1105-`date +%Y%m%d%H%M%S`
+FILENAMEN=e10-next-`date +%Y%m%d%H%M%S`
+echo "Creating $FILENAMES.zip ..."
 cd $ufilesd
-zip -rD $uarchived/$FILENAME ./*
-echo sending file to bluehost
-scp $uarchived/$FILENAME.zip iworryfo@jcwoltz.com:public_html/e10/
+zip -rD $uarchived/$FILENAMES ./*
+
+echo "======================================================================"
+echo "======================================================================"
+echo
+echo "Copying E10-Next files..."
+cp $nkernel $ufilesd/
+cp $nrootfs $ufilesd/
+echo "Creating $FILENAMEN.zip ..."
+zip -rD $uarchived/$FILENAMEN ./*
+echo "======================================================================"
+echo "======================================================================"
+
+#echo sending file to bluehost
+#scp $uarchived/$FILENAME.zip iworryfo@jcwoltz.com:public_html/e10/
 echo "Done!"
 
