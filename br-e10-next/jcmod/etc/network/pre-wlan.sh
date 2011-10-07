73d92eda263236c213a2d6e50663c35fb9810e84
J.C. Woltz <jwoltz@gmail.com>
2011-10-07 09:56:48 -0400

    update buildroot config to use linux config from boards dir update startup scripts. Add cron and led scripts update root crontab to comment out default scripts

--- br-e10-next/jcmod/etc/network/pre-wlan.sh
+++ br-e10-next/jcmod/etc/network/pre-wlan.sh
@@ -12,34 +12,34 @@
 if [ $? -eq 0 ]
 then
 
-COUNT=0
-echo "Starting wpa_supplicant..."
-/usr/sbin/wpa_supplicant -Dwext -iwlan0 -c/etc/wpa_supplicant.conf -B
-echo "wpa_supplicant exit code: $?"
-until [ $COUNT -gt 30 ]; do
-  sleep 1
-  let COUNT=COUNT+1
-  WPASTAT=$(/usr/sbin/wpa_cli -i wlan0 status | grep wpa_state)
-  if [ "$WPASTAT" = "wpa_state=COMPLETED" ]
-  then
-    let COUNT=COUNT+100
-    echo $COUNT WPA_STATUS Connected
-    break
-  else
-    if [ "$WPASTAT" = "wpa_state=SCANNING" ]
+  COUNT=0
+  echo "Starting wpa_supplicant..."
+  /usr/sbin/wpa_supplicant -Dwext -iwlan0 -c/etc/wpa_supplicant.conf -B
+  echo "wpa_supplicant exit code: $?"
+  until [ $COUNT -gt 30 ]; do
+    sleep 1
+    let COUNT=COUNT+1
+    WPASTAT=$(/usr/sbin/wpa_cli -i wlan0 status | grep wpa_state)
+    if [ "$WPASTAT" = "wpa_state=COMPLETED" ]
     then
-      echo Scanning $COUNT
+      let COUNT=COUNT+100
+      echo $COUNT WPA_STATUS Connected
+      break
     else
-      echo $WPASTAT ... $COUNT
+      if [ "$WPASTAT" = "wpa_state=SCANNING" ]
+      then
+        echo Scanning $COUNT
+      else
+        echo $WPASTAT ... $COUNT
+      fi
+      #/usr/sbin/wpa_cli -i wlan0 status
+      #echo $? wpa_cli exit code
     fi
-    #/usr/sbin/wpa_cli -i wlan0 status
-    #echo $? wpa_cli exit code
-  fi
-done
+  done
 
-HOSTNAME=`cat /etc/hostname`
-echo /sbin/udhcpc -i wlan0 --hostname $HOSTNAME -p /var/run/udhcpc-wlan0.pid --background --syslog --arping
-/sbin/udhcpc -i wlan0 --hostname $HOSTNAME -p /var/run/udhcpc-wlan0.pid --background --syslog --arping
+  HOSTNAME=`cat /etc/hostname`
+  echo /sbin/udhcpc -i wlan0 --hostname $HOSTNAME -p /var/run/udhcpc-wlan0.pid --background --syslog --arping
+  /sbin/udhcpc -i wlan0 --hostname $HOSTNAME -p /var/run/udhcpc-wlan0.pid --background --syslog --arping
 
 else
   echo "wlan0 not found"
