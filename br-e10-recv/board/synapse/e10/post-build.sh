bcc9597bb25f29cc05677b28d98944d9d5eb0c53
J.C. Woltz <jwoltz@gmail.com>
2011-11-04 16:38:33 -0400

    fixing new dirs

--- br-e10-recv/board/synapse/e10/post-build.sh
+++ br-e10-recv/board/synapse/e10/post-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo `/bin/date` $1 $2 >> ~/jtmp
-cp -av jcmod/* $1 >> ~/jtmp
+cp -av  board/synapse/e10/root-additions/* $1 >> ~/jtmp
 echo `/bin/date` $1 $2 >> ~/jtmp
 date -u +%Y%m%d%H%M%S > $1/etc/jc-version
 chmod 755 $1/etc/init.d/*
