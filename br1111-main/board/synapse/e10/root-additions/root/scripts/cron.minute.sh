bff7c71cd4a5c730464c79f6a903a68f22dcb01d
J.C. Woltz <jwoltz@gmail.com>
2011-12-17 14:02:45 -0500

    S999snap check for snap connect cron.minute to check wireless every minute update-jcu to download snap connect crontab to enable cron.minute

--- br1111-main/board/synapse/e10/root-additions/root/scripts/cron.minute.sh
+++ br1111-main/board/synapse/e10/root-additions/root/scripts/cron.minute.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 
+/root/check-wlan.sh
--- br1111-main/board/synapse/e10/root-additions/root/scripts/update-jcu.sh
+++ br1111-main/board/synapse/e10/root-additions/root/scripts/update-jcu.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cd /root
+wget http://jcu.homelinux.org/jc/Snap-3.0.3-py2.7.tgz
+tar -xzvf Snap-3.0.3-py2.7.tgz
+mv Snap-3.0.3-py2.7 Snap
 scp -P 22174 stfu@jcu.homelinux.org:UserMain.py /root/UserMain.py
 chown root:root /root/UserMain.py
 chmod 755 /root/UserMain.py
