99c3db2ab6d504544d478d723e193f151603ad0f
J.C. Woltz <jwoltz@gmail.com>
2011-12-18 13:59:30 -0500

    update config update update-jcu.sh to download snapconnect 3.0.3 from synapse

--- br1111-main/board/synapse/e10/root-additions/root/scripts/update-jcu.sh
+++ br1111-main/board/synapse/e10/root-additions/root/scripts/update-jcu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /root
-wget http://jcu.homelinux.org/jc/Snap-3.0.3-py2.7.tgz
+wget http://forums.synapse-wireless.com/upload/Snap-3.0.3-py2.7.tgz
 tar -xzvf Snap-3.0.3-py2.7.tgz
 mv Snap-3.0.3-py2.7 Snap
 scp -P 22174 stfu@jcu.homelinux.org:UserMain.py /root/UserMain.py
