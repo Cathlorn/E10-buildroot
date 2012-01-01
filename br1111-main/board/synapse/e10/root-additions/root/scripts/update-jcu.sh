526da1563c74a59458f1af9b08fbc573197ed514
J.C. Woltz <jwoltz@gmail.com>
2012-01-01 13:09:15 -0500

    Add/update update-jcu.sh to prep *MY* E10 for my use after fresh install Deleted UserMain from distro fixed errors in snmpd and lighttpd congig small touchup to index.html

--- br1111-main/board/synapse/e10/root-additions/root/scripts/update-jcu.sh
+++ br1111-main/board/synapse/e10/root-additions/root/scripts/update-jcu.sh
@@ -4,6 +4,12 @@ cd /root
 wget http://forums.synapse-wireless.com/upload/Snap-3.0.3-py2.7.tgz
 tar -xzvf Snap-3.0.3-py2.7.tgz
 mv Snap-3.0.3-py2.7 Snap
-scp -P 22174 stfu@jcu.homelinux.org:UserMain.py /root/UserMain.py
+wget http://www.jcwoltz.com/e10/UserMain.py
 chown root:root /root/UserMain.py
 chmod 755 /root/UserMain.py
+wget http://www.jcwoltz.com/e10/createSNAPSQLlite.txt
+/usr/bin/sqlite3 dc.sqlite < createSNAPSQLlite.txt
+mkdir sc
+/bin/sync
+/sbin/reboot
+
